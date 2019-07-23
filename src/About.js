ca9111bd728682180ce901d7b7afec2359e21e41
JoshNas <36458322+JoshNas@users.noreply.github.com>
2019-07-23 18:58:48 -0400

    fluid profile image

--- src/About.js
+++ src/About.js
@@ -34,7 +34,7 @@ changeBackground () {
       <div className="jumbotron jumbotron-fluid paral" id="about" style={this.state.originalBackground ? background1 : background2} onClick={this.changeBackground.bind(this)}>
         <div className="container text-center">
           <h1 className="display-2">{info.name}</h1>
-          <img src={profilepic} className="rounded-circle" alt="profile" />
+          <img src={profilepic} className="rounded-circle img-fluid" alt="profile" />
           <h1 class="display-2">{info.title}</h1>
           <h2 class="display-3">{info.specializiations}</h2>
           <p class="display-5">{info.skillOne}</p>
