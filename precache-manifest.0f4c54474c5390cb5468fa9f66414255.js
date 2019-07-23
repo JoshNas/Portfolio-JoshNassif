405b4d971adf07734c8bac4d7864d021b53285fc
JoshNas <36458322+JoshNas@users.noreply.github.com>
2019-07-23 17:37:40 -0400

    Updates

---
+++ precache-manifest.0f4c54474c5390cb5468fa9f66414255.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680e38c62e4ecd359e793d127aa65fe0",
+    "url": "/Portfolio-JoshNassif/index.html"
+  },
+  {
+    "revision": "1e2c81d5761d17cb573b",
+    "url": "/Portfolio-JoshNassif/static/css/main.02e18c05.chunk.css"
+  },
+  {
+    "revision": "ee8509e418c9171baad3",
+    "url": "/Portfolio-JoshNassif/static/js/2.ed9a8b38.chunk.js"
+  },
+  {
+    "revision": "1e2c81d5761d17cb573b",
+    "url": "/Portfolio-JoshNassif/static/js/main.aca16dfc.chunk.js"
+  },
+  {
+    "revision": "121500ec0de8afd87cb3",
+    "url": "/Portfolio-JoshNassif/static/js/runtime~main.c1280c89.js"
+  },
+  {
+    "revision": "2ac43b93be5bd1910be480ec91151a1e",
+    "url": "/Portfolio-JoshNassif/static/media/background.2ac43b93.jpg"
+  },
+  {
+    "revision": "394ac3613c33f94ed662d3e246775eb1",
+    "url": "/Portfolio-JoshNassif/static/media/background2.394ac361.jpg"
+  },
+  {
+    "revision": "f2d236a54cf2be259beb0a83efab1b6b",
+    "url": "/Portfolio-JoshNassif/static/media/background2Alt.f2d236a5.gif"
+  },
+  {
+    "revision": "29ea856bf44eae16ba7f9b005e7b2f61",
+    "url": "/Portfolio-JoshNassif/static/media/background3.29ea856b.jpg"
+  },
+  {
+    "revision": "00cfc3aa9b5ec7a6c612347465a83fbb",
+    "url": "/Portfolio-JoshNassif/static/media/profile.00cfc3aa.jpg"
+  },
+  {
+    "revision": "2470c2f94f1bf7398732b1d61f20d9b6",
+    "url": "/Portfolio-JoshNassif/static/media/projectOne1.2470c2f9.png"
+  },
+  {
+    "revision": "14000182ff5b0e69906c034dc6898b52",
+    "url": "/Portfolio-JoshNassif/static/media/projectOne2.14000182.png"
+  },
+  {
+    "revision": "e0c6ec434273d01c5aea171e448fa2fc",
+    "url": "/Portfolio-JoshNassif/static/media/projectOne3.e0c6ec43.png"
+  },
+  {
+    "revision": "484c825b03922b8bdcb15c06b3753c63",
+    "url": "/Portfolio-JoshNassif/static/media/projectOneBig1.484c825b.png"
+  },
+  {
+    "revision": "83ccf8e803021023741560a12f569ae9",
+    "url": "/Portfolio-JoshNassif/static/media/projectOneBig2.83ccf8e8.png"
+  },
+  {
+    "revision": "4ae3046c5ed7d469caa0c08857ec1d1c",
+    "url": "/Portfolio-JoshNassif/static/media/projectOneBig3.4ae3046c.png"
+  },
+  {
+    "revision": "bd604196e4afed8b4bb7ef47735a5316",
+    "url": "/Portfolio-JoshNassif/static/media/projectThree1.bd604196.png"
+  },
+  {
+    "revision": "0821a60577cc498874291e0db066677d",
+    "url": "/Portfolio-JoshNassif/static/media/projectThree2.0821a605.png"
+  },
+  {
+    "revision": "eba801d2fbd2cf2511c384fc7824ba05",
+    "url": "/Portfolio-JoshNassif/static/media/projectThree3.eba801d2.png"
+  },
+  {
+    "revision": "980124cea1dcf96a07500e05beec4732",
+    "url": "/Portfolio-JoshNassif/static/media/projectThreeBig1.980124ce.png"
+  },
+  {
+    "revision": "dac5b845cfcbdc912d24591a5b5650d4",
+    "url": "/Portfolio-JoshNassif/static/media/projectThreeBig2.dac5b845.png"
+  },
+  {
+    "revision": "8355072e189ee7a0f0606ed7608dd313",
+    "url": "/Portfolio-JoshNassif/static/media/projectThreeBig3.8355072e.png"
+  },
+  {
+    "revision": "9cd3a739fa30956c874a18c8da309cff",
+    "url": "/Portfolio-JoshNassif/static/media/projectTwo1.9cd3a739.png"
+  },
+  {
+    "revision": "87cf7b763ed86be03e3c4873ec6b93a8",
+    "url": "/Portfolio-JoshNassif/static/media/projectTwo2.87cf7b76.png"
+  },
+  {
+    "revision": "6b30e3f0e211c103bd542ab471eb1504",
+    "url": "/Portfolio-JoshNassif/static/media/projectTwo3.6b30e3f0.png"
+  },
+  {
+    "revision": "777b83e858da52eff0e5b3e6bc46afc2",
+    "url": "/Portfolio-JoshNassif/static/media/projectTwoBig1.777b83e8.png"
+  },
+  {
+    "revision": "66929368897114c3a77b6d2c3f56ee3e",
+    "url": "/Portfolio-JoshNassif/static/media/projectTwoBig2.66929368.png"
+  },
+  {
+    "revision": "39b94d8b0779c19419e2be89ed940fab",
+    "url": "/Portfolio-JoshNassif/static/media/projectTwoBig3.39b94d8b.png"
+  }
+]);
